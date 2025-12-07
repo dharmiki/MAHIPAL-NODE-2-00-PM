473d5727942e7212efb880114ae43e2660dd6baa
dharmiki <dharmikiparmar@gmail.com>
2025-12-07 18:42:13 +0530

    Books

---
+++ Project-4(Book)/model/schma.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+
+    author : {
+        type : String,
+        required : true
+    },
+
+    price :{
+        type : Number,
+        required : true
+    }
+})
+
+let fschema = mongoose.model("book",schema)
+
+module.exports = fschema
